78b8fabac0ccf481e3281d46c2f37b11b53f738f
Arthur Augusto <arthuraasantos@hotmail.com>
2018-04-03 23:19:46 -0300

    Inicio do projeto

---
+++ AdvRealSl/Web/Controllers/ProcessosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class ProcessosController : Controller
+    {
+        public IActionResult Lista()
+        {
+            return View();
+        }
+    }
+}

9b392b9b6adbac61967cba080184d60ce77cf45c
Arthur Augusto <arthuraasantos@hotmail.com>
2018-04-10 00:01:50 -0300

    Menu de tarefas

--- AdvRealSl/Web/Controllers/EscritorioController.cs
+++ AdvRealSl/Web/Controllers/EscritorioController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers
 {
@@ -14,6 +10,16 @@ namespace Web.Controllers
         }
 
         public IActionResult Perfil()
+        {
+            return View();
+        }
+
+        public IActionResult Agenda()
+        {
+            return View();
+        }
+
+        public IActionResult Tarefas()
         {
             return View();
         }

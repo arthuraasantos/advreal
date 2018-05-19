9379c14348469b3936f9501dcce9f79a90ddaf5b
Arthur Augusto <arthuraasantos@hotmail.com>
2018-05-19 11:41:31 -0300

    tirando debug

--- AdvRealSl/Web/Startup.cs
+++ AdvRealSl/Web/Startup.cs
@@ -62,10 +62,10 @@ namespace Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            app.UseDeveloperExceptionPage();
-            //}
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
             app.UseStaticFiles();
             app.UseAuthentication();
